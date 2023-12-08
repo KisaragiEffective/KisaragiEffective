23791cda0ffd7898c899aa27c8d9c8b45a549d10
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-12-08 09:43:53 +0900

    fix: fix incorrect extension sequence

--- scripts/render_live.sh
+++ scripts/render_live.sh
@@ -4,7 +4,7 @@ readonly this="$(dirname "$0")"
 readonly root="$(realpath "$this/..")"
 
 if [[ -z "$RENDER_TO" ]]; then
-  RENDER_TO="$root/output/README.template.md"
+  RENDER_TO="$root/output/README.md"
 fi
 
 "$root/markdown-template-preprocessor" -i "$root/README.template.md" -o "$RENDER_TO" -m spoiler
