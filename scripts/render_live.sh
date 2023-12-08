b3b2aa2cf44aa01a82efe98520ded173ef70b87e
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-12-08 09:49:18 +0900

    fix: fix unnecessary absolute path

--- scripts/render_live.sh
+++ scripts/render_live.sh
@@ -7,4 +7,9 @@ if [[ -z "$RENDER_TO" ]]; then
   RENDER_TO="$root/output/README.md"
 fi
 
-"$root/markdown-template-preprocessor" -i "$root/README.template.md" -o "$RENDER_TO" -m spoiler
+# HACK: MTP resolves each component to be relative path based on PWD, not from the root document.
+#       It causes summary of spoiler to be like `./scripts/../components/<whatever>`, which is not so elegant.
+#       This requires modification of MTP, but we can work-around it by changing PWD before invoking it.
+cp "$root/README.template.md" .
+"$root/markdown-template-preprocessor" -i "./README.template.md" -o "$RENDER_TO" -m spoiler
+rm "$root/README.template.md"
