eb000e1d6295b7f9d3d5ef5231cb17284f0971c5
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-12-08 09:15:06 +0900

    fix: set PWD before invoke MTP

--- scripts/render_live.sh
+++ scripts/render_live.sh
@@ -2,4 +2,9 @@
 
 readonly this="$(dirname "$0")"
 readonly root="$this/.."
-"$root/markdown-template-preprocessor" -i "$root/README.template.md" -o "$root/output/README.md" -m spoiler
+
+# HACK: MTP resolves each component to be relative path based on PWD, not from the root document.
+#       It causes summary of spoiler to be like `./scripts/../components/<whatever>`, which is not so elegant.
+#       This requires modification of MTP, but we can work-around it by changing PWD before invoking it.
+# shellcheck disable=SC2164
+(cd "$root"; "$root/markdown-template-preprocessor" -i "$root/README.template.md" -o "$root/output/README.md" -m spoiler)
